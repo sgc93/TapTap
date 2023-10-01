d5be4c8f0774d4d164719a5c62a44ad9da1d5d1d
sgc93 <smachewgedefaw@gmail.com>
2023-09-30 22:08:03 -0900

    create drawer menu

---
+++ lib/pages/drawer_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.grey[900],
+      child: const Text(
+        'DRAWER',
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
