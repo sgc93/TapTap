6956ed3360a67a1f21beca36e096c5a56db1e6b9
sgc93 <smachewgedefaw@gmail.com>
2023-10-01 11:18:29 -0900

    styling customized reusable container

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -14,11 +14,11 @@ class MyContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.all(20),
+      margin: const EdgeInsets.all(15),
       height: height,
       width: width,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
+        borderRadius: BorderRadius.circular(20),
         // shape: BoxShape.circle,
         color: Colors.grey[900],
         boxShadow: [
@@ -26,12 +26,12 @@ class MyContainer extends StatelessWidget {
             color: Colors.black,
             offset: Offset(4, 4),
             blurRadius: 5,
-            spreadRadius: 1,
+            // spreadRadius: 10,
           ),
           BoxShadow(
             color: Colors.grey.shade800,
             offset: const Offset(-4, -4),
-            blurRadius: 8,
+            blurRadius: 5,
             spreadRadius: 1,
           ),
         ],
