9704049561227463e8f793ba5b5afb1391336928
sgc93 <smachewgedefaw@gmail.com>
2023-09-30 21:41:14 -0900

    add buttons and title to appBar

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -12,15 +12,65 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
+      height: height,
+      width: width,
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           MyContainer(
-            height: height * 0.5,
-            width: height * 0.5,
-            child: Icon(Icons.back_hand),
+            height: height * 0.7,
+            width: height * 0.7,
+            child: drawerDisplayerButton(),
+          ),
+          titleColumn(),
+          MyContainer(
+            height: height * 0.7,
+            width: height * 0.7,
+            child: endDrawerDisplayerButton(),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget titleColumn() {
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'TapTap',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+        ),
+        Text(
+          'tap more to win',
+          style: TextStyle(
+            color: Colors.white38,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget drawerDisplayerButton() {
+    return GestureDetector(
+      onTap: () {},
+      child: const Icon(
+        Icons.menu,
+        color: Colors.white,
+      ),
+    );
+  }
+
+  Widget endDrawerDisplayerButton() {
+    return GestureDetector(
+      onTap: () {},
+      child: const Icon(
+        Icons.abc,
+        color: Colors.white,
       ),
     );
   }
