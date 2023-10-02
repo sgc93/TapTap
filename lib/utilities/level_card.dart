33a01b7b1939fad7646bdbb5a640678a87f7a109
sgc93 <smachewgedefaw@gmail.com>
2023-10-02 10:35:20 -0900

    create ui components for level card

--- lib/utilities/level_card.dart
+++ lib/utilities/level_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tab_tab_game/widgets/container.dart';
 
 class LevelCard extends StatefulWidget {
   const LevelCard({super.key});
@@ -8,8 +9,66 @@ class LevelCard extends StatefulWidget {
 }
 
 class _LevelCardState extends State<LevelCard> {
+  final int _level = 1;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        const Text(
+          'LEVEL',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 35,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.left,
+        ),
+        Text(
+          '$_level',
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 100,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            MyContainer(
+              height: 60,
+              width: 60,
+              child: GestureDetector(
+                onTap: () {},
+                child: const Center(
+                  child: Text(
+                    '-',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            MyContainer(
+              height: 60,
+              width: 60,
+              child: GestureDetector(
+                onTap: () {},
+                child: const Center(
+                  child: Text(
+                    '+',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
