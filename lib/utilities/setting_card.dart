b5e288f0152a34aa3114be6a167b59c7de5b61e6
sgc93 <smachewgedefaw@gmail.com>
2023-10-02 11:01:03 -0900

    create sidebar setting card

---
+++ lib/utilities/setting_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsCard extends StatefulWidget {
+  const SettingsCard({super.key});
+
+  @override
+  State<SettingsCard> createState() => _SettingsCardState();
+}
+
+class _SettingsCardState extends State<SettingsCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
