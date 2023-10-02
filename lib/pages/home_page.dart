a43995255affb350122cd7b377587face51e0643
sgc93 <smachewgedefaw@gmail.com>
2023-10-02 10:45:09 -0900

    add customized level card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:tab_tab_game/pages/drawer_page.dart';
 import 'package:tab_tab_game/pages/end_rawer_page.dart';
+import 'package:tab_tab_game/utilities/duration_card.dart';
+import 'package:tab_tab_game/utilities/level_card.dart';
 import 'package:tab_tab_game/widgets/app_bar.dart';
 import 'package:tab_tab_game/widgets/container.dart';
 
@@ -38,26 +40,12 @@ class _HomePageState extends State<HomePage> {
                     MyContainer(
                       height: _height * 0.36,
                       width: _width * 0.6,
-                      child: const Center(
-                        child: Text(
-                          'LEVEL',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      child: const LevelCard(),
                     ),
                     MyContainer(
                       height: _height * 0.36,
                       width: _width * 0.6,
-                      child: const Center(
-                        child: Text(
-                          'DURATION',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                      child: const DurationCard(),
                     ), // dutation box
                   ],
                 ),
