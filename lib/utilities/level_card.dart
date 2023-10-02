ccb7f3d744b9220fb3a27521dce92456502296dc
sgc93 <smachewgedefaw@gmail.com>
2023-10-02 10:09:19 -0900

    creating a customized widget for level

---
+++ lib/utilities/level_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LevelCard extends StatefulWidget {
+  const LevelCard({super.key});
+
+  @override
+  State<LevelCard> createState() => _LevelCardState();
+}
+
+class _LevelCardState extends State<LevelCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
