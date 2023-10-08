e8d07536cec698589d006823f563a66f75e0d184
sgc93 <smachewgedefaw@gmail.com>
2023-10-08 03:36:54 -0900

    add fields

--- lib/model/settings.dart
+++ lib/model/settings.dart
@@ -2,6 +2,25 @@ mixin Settings {
   int level = 1;
   int duration = 20;
 
+  final double statusConstant = 10 / 80;
+  final double heightConstant = 80 / 10;
+
+  bool isGameStarted = false;
+  bool isGamePoused = false;
+  bool isgameOver = false;
+
+  double gameAreaOneHeight = 400;
+  double gameAreaTwoHeight = 400;
+
+  int playerOneStatus = 50;
+  int playerTwoStatus = 50;
+
+  int playerOneNumTap = 0;
+  int playerTwoNumTap = 0;
+
+  bool isPlayerOneWin = false;
+  bool isPlayerTwoWin = false;
+
   void setLevel({required newLevel}) {
     level = newLevel;
   }
